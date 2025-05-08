bcc4433c40913fe781d22bf3ba3b80f7450d0cd6
Rnveer007 <rvs523474@gmail.com>
2025-05-08 10:16:04 +0530

    updated

--- src/TodoRedux.jsx
+++ src/TodoRedux.jsx
@@ -1,9 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux'
-import { setInput, addTask } from './slices/todoslice'
+import { setInput, addTask, deleteTask, editTask } from './slices/todoslice'
+import { MdDelete } from "react-icons/md";
+import { MdEdit } from "react-icons/md";
+
+
 function TodoRedux() {
     const dispatch = useDispatch()
     const initState = useSelector(state => state.todoReducer)
-    console.log(initState)
+    // console.log(initState)
     return (
         <>
             <input
@@ -17,13 +21,13 @@ function TodoRedux() {
                 Add Task
             </button>
             <ul>
-                {initState.task.map((obj) =>
-                (
+                {initState.task.map((obj) => (
                     <li key={obj.id}>
                         {obj.text}
+                        <span onClick={() => dispatch(deleteTask(obj.id))}><MdDelete /></span>
+                        <span onClick={() => dispatch(editTask(obj.id))}><MdEdit /></span>
                     </li>
-                )
-                )}
+                ))}
             </ul>
         </>
     )
--- src/slices/todoslice.js
+++ src/slices/todoslice.js
@@ -1,24 +1,39 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const todoSl = createSlice({
-    name: "todoSlice",
+    name: "todoList",
     initialState: {
         input: "",
-        task: []
+        task: [],
+        isEditing: false,
+        editId: null
     },
     reducers: {
         setInput: function (state, action) {
             state.input = action.payload
         },
         addTask: function (state) {
-            state.task.push({
-                id: Date.now(),
-                task: state.input,
-            })
-            state.input = ""
-
+            state.isEditing ?
+                (state.task = state.task.map((item) =>
+                    item.id === state.editId
+                        ? { ...state.task, text: state.input }
+                        : item
+                ))
+                : (state.task = [...state.task, { id: Date.now(), text: state.input }]);
+            state.isEditing = false;
+            state.editId = null;
+            state.input = "";
+        },
+        deleteTask: function (state, action) {
+            state.task = state.task.filter((obj) => obj.id !== action.payload)
+        },
+        editTask: function (state, action) {
+            const taskToEdit = state.task.find((item) => item.id === action.payload);
+            state.input = taskToEdit.text
+            state.isEditing = true;
+            state.editId = action.payload
         }
     }
 })
 export const todoReducer = todoSl.reducer;
-export const { setInput, addTask } = todoSl.actions;
+export const { setInput, addTask, deleteTask, editTask } = todoSl.actions;
